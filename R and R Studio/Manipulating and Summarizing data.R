80f1f3be6048ab8a2857e205fd489ef24e8627ca
charles mawusi <mawusicharles@yahoo.com>
2022-04-13 12:22:17 +0100

    Rdatabes

--- R and R Studio/Manipulating and Summarizing data.R
+++ R and R Studio/Manipulating and Summarizing data.R
@@ -15,3 +15,13 @@ countryafrica<-c("Ghana", "Nigeria", " Togo", "Gabon")
 
 gapminder %>% filter(country %in% countryafrica & year >=2000) %>% head()
 
+# sorting data 
+
+xed<-gapminder %>% filter(year>=2000) %>%
+  group_by(continent) %>% 
+  summarize(across(c(lifeExp, pop,gdpPercap),  list(avg=~mean(.))) ) %>% 
+  arrange(continent, desc(gdpPercap_avg)) 
+xed %>% select(gdpPercap_avg, continent) %>%
+  as.numeric(xed$gdpPercap_avg) %>% hist()
+
+
